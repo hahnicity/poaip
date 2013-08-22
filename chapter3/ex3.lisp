d6d3f1e3941b29fbe5a12c334f68c9f107941066
hahnicity <grehm87@gmail.com>
2013-08-21 23:15:24 -0700

    Fixed up ex3.lisp, got it working Tested with input '(1 2 (3 3 4))

--- chapter3/ex3.lisp
+++ chapter3/ex3.lisp
@@ -1,14 +1,13 @@
 ;;;; ex3.lisp
-;;;; Not working... will need to fix this up
 
 (in-package #:chapter3)
 
 (defun print-dot-notation (expr)
-  (print expr)
+  (let ((dot-notation nil)))
   (cond
-    ((null expr) ())
-    ((listp (cdr expr)) (princ (format nil "~S.~S" (car expr) (print-dot-notation (cdr expr)))))
-    (t (princ (format nil "~S.~S" (car expr) (cdr expr))))
+    ((null (cdr expr)) (setf dot-notation (format nil "(~S . ~S)" (car expr) nil)))
+    ((listp (cdr expr)) (setf dot-notation (format nil "(~S . (~S))" (car expr) (print-dot-notation (cdr expr)))))
+    (t (princ (dot-notation)))
   )
 )
 
